e5349d95c0cb1f6fd9629422e897f72e753472d9
282207134 <liu282207134@yahoo.co.jp>
2026-03-28 00:29:24 +0900

    更新项目配置文件和测试文件

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,21 +7,24 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'package:library_management/main.dart';
+import 'package:shimmer/main.dart';
 
 void main() {
-  testWidgets('App launches successfully', (WidgetTester tester) async {
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(
-      const ProviderScope(
-        child: LibraryManagementApp(),
-      ),
-    );
+    await tester.pumpWidget(const MyApp());
 
-    // Verify that the app has loaded
-    await tester.pumpAndSettle();
-    expect(find.byType(MaterialApp), findsOneWidget);
+    // Verify that our counter starts at 0.
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
+
+    // Tap the '+' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
+
+    // Verify that our counter has incremented.
+    expect(find.text('0'), findsNothing);
+    expect(find.text('1'), findsOneWidget);
   });
 }
